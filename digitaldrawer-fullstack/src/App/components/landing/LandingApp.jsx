8d5b409348e35c08a978ac1625ebc48a4961292f
Josh Beatty <joshuabeatty88@gmail.com>
2020-10-25 22:34:09 -0600

    Removed incorrect digitaldrawer dir

--- digitaldrawer-fullstack/src/App/components/landing/LandingApp.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const LandingApp = () => {
-        return (
-            <div>
-                <h1>Welcome to this super ugly landing page!</h1>
-            </div>
-        )
-}
