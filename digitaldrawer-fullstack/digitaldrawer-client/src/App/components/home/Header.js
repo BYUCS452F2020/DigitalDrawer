edd22883c911e40207581a274ffef1f5e1b00899
Josh Beatty <joshuabeatty88@gmail.com>
2020-12-06 06:25:52 -0700

    Restyle the header and jumbotron. Hope you like rainbows

--- digitaldrawer-fullstack/digitaldrawer-client/src/App/components/home/Header.js
+++ digitaldrawer-fullstack/digitaldrawer-client/src/App/components/home/Header.js
@@ -8,6 +8,8 @@ const Header = ({history, handleSubmit}) => {
         <Jumbotron>
             <div>
                 <h1 className="digitaldrawer-head">DigitalDrawer</h1>
+                <h4 className="head_descrip">Manually add bookmarks with the "+Add" button below or install the DigitalDrawer plugin for browser integration! </h4>
+                <h4 className="head_descrip">Search below for bookmarks you've already saved or bookmarks others have saved!</h4>
                 <Form history={history} handleSubmit={handleSubmit}/>
                 <Navigation/>
             </div>
