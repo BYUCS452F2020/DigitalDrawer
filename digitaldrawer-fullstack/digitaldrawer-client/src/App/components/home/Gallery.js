04ebf77ad02d2c537af1f5a1f5c124b76c08a90e
Josh Beatty <joshuabeatty88@gmail.com>
2020-12-06 06:25:20 -0700

    Rename to match context and pass new data to card

--- digitaldrawer-fullstack/digitaldrawer-client/src/App/components/home/Gallery.js
+++ digitaldrawer-fullstack/digitaldrawer-client/src/App/components/home/Gallery.js
@@ -1,28 +1,30 @@
 import React from "react";
-import NoImages from "./NoImages";
-import Image from "./Image";
+import NoBookmarks from "./NoBookmarks";
+import BookmarkReviewCard from "./BookmarkReviewCard";
 const Gallery = props => {
   const results = props.data;
-  let images;
-  let noImages;
-  // map variables to each item in fetched image array and return image component
+  let bookmarks;
+  let noBookmarks;
+
+  // TODO: replace this with call to DB that gets the URL and then search for either a random image or image that fits the url
+  // FIXME: The info that will be displayed needs to come from here. We can pass in the url, img, notes, rating, etc.
   if (results.length > 0) {
-    images = results.map(image => {
+    bookmarks = results.map(image => {
       let farm = image.farm;
       let server = image.server;
       let id = image.id;
       let secret = image.secret;
       let title = image.title;
       let url = `https://farm${farm}.staticflickr.com/${server}/${id}_${secret}_m.jpg`;
-      return <Image url={url} key={id} alt={title} />;
+      return <BookmarkReviewCard url={url} key={id} alt={title} date={new Date().toDateString()} />;
     });
   } else {
-    noImages = <NoImages />; // return 'not found' component if no images fetched
+    noBookmarks = <NoBookmarks />; // return 'not found' component if no images fetched
   }
   return (
     <div>
-      <ul>{images}</ul>
-      {noImages}
+      <ul>{bookmarks.slice(1, 4)}</ul>
+      {noBookmarks}
     </div>
   );
 };
