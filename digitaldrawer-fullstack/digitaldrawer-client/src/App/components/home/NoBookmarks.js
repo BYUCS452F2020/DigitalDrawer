72dea3518b50b03d13ce5a980105442ae02cd553
Josh Beatty <joshuabeatty88@gmail.com>
2020-12-06 06:32:36 -0700

    Rename to fit context

---
+++ digitaldrawer-fullstack/digitaldrawer-client/src/App/components/home/NoBookmarks.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const NoBookmarks = () => (
+  <div>
+    <h2>No Bookmarks Found</h2>
+    <p>Try searching for something else!</p>
+  </div>
+);
+
+export default NoBookmarks;
