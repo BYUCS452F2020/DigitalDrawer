807755485894bbbbe78eac1150fb4055bd6e6725
Josh Beatty <joshuabeatty88@gmail.com>
2020-12-06 19:50:44 -0700

    Replace jumbotron with app bar

--- digitaldrawer-fullstack/digitaldrawer-client/src/App/components/home/Header.js
+++ digitaldrawer-fullstack/digitaldrawer-client/src/App/components/home/Header.js
@@ -2,19 +2,192 @@ import React from "react";
 import Form from "./Form";
 import Navigation from "./Navigation";
 import Jumbotron from "react-bootstrap/Jumbotron";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+import {fade} from "@material-ui/core";
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MailIcon from '@material-ui/icons/Mail';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import MoreIcon from '@material-ui/icons/MoreVert';
+import Badge from "@material-ui/core/Badge";
+import InputBase from "@material-ui/core/InputBase";
+import Typography from "@material-ui/core/Typography";
+import Toolbar from "@material-ui/core/Toolbar";
+import AppBar from "@material-ui/core/AppBar";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import IconButton from '@material-ui/core/IconButton';
 
-const Header = ({history, handleSubmit}) => {
-    return (
-        <Jumbotron>
-            <div>
-                <h1 className="digitaldrawer-head">DigitalDrawer</h1>
-                <h4 className="head_descrip">Manually add bookmarks with the "+Add" button below or install the DigitalDrawer plugin for browser integration! </h4>
-                <h4 className="head_descrip">Search below for bookmarks you've already saved or bookmarks others have saved!</h4>
-                <Form history={history} handleSubmit={handleSubmit}/>
-                <Navigation/>
-            </div>
-        </Jumbotron>
+
+const useStyles = makeStyles((theme) => ({
+    grow: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginRight: theme.spacing(2),
+    },
+    title: {
+        display: 'none',
+        [theme.breakpoints.up('sm')]: {
+            display: 'block',
+        },
+    },
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginRight: theme.spacing(2),
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing(3),
+            width: 'auto',
+        },
+    },
+    searchIcon: {
+        padding: theme.spacing(0, 2),
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        color: 'inherit',
+    },
+    inputInput: {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+            width: '20ch',
+        },
+    },
+    sectionDesktop: {
+        display: 'none',
+        [theme.breakpoints.up('md')]: {
+            display: 'flex',
+        },
+    },
+    sectionMobile: {
+        display: 'flex',
+        [theme.breakpoints.up('md')]: {
+            display: 'none',
+        },
+    },
+}));
+
+export default function Header({history, handleSubmit}) {
+    const classes = useStyles();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const isMenuOpen = Boolean(anchorEl);
+
+    const handleProfileMenuOpen = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+        alert("Closing menu")
+    };
+
+    const menuId = 'primary-search-account-menu';
+
+    function handleProfileClick() {
+        //TODO: Send to a profile page
+        alert("User: DigitalDrawer Dev\nPermissions: All\nProfile is under maintenance, please try again later")
+    }
+
+    function handleAccountClick() {
+        //TODO: Send to account settings
+        alert("Account Settings is under maintenance, please try again later ")
+    }
+
+    const renderMenu = (
+        <Menu
+            anchorEl={anchorEl}
+            anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+            id={menuId}
+            keepMounted
+            transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+            open={isMenuOpen}
+            onClose={handleMenuClose}
+        >
+            <MenuItem onClick={handleProfileClick}>Profile</MenuItem>
+            <MenuItem onClick={handleAccountClick}>My account</MenuItem>
+        </Menu>
     );
-};
 
-export default Header;
+    function handleOpenMessages() {
+        //TODO: Send to messages page
+        alert("Messages is under maintenance,  please try again later")
+    }
+
+    function handleOpenNotifications() {
+        //TODO: Send to notifications page
+        alert("Notifications is under maintenance,  please try again later")
+    }
+
+    return (
+        <div className={classes.grow}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        edge="start"
+                        className={classes.menuButton}
+                        color="inherit"
+                        aria-label="open drawer"
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography className={classes.title} variant="h6" noWrap>
+                        DigitalDrawer
+                    </Typography>
+                    <div className={"search-bar"}>
+                        <InputBase
+                            classes={{
+                                root: classes.inputRoot,
+                                input: classes.inputInput,
+                            }}
+                            inputProps={{ 'aria-label': 'search' }}
+                            onSubmit={handleSubmit}
+                        />
+                        <Form history={history} handleSubmit={handleSubmit}/>
+                    </div>
+                    <div className={classes.grow} />
+                    <div className={classes.sectionDesktop}>
+                        <IconButton aria-label="show 4 new mails" color="inherit">
+                            <Badge badgeContent={4} color="secondary">
+                                <MailIcon onClick={handleOpenMessages}/>
+                            </Badge>
+                        </IconButton>
+                        <IconButton aria-label="show 17 new notifications" color="inherit">
+                            <Badge badgeContent={17} color="secondary">
+                                <NotificationsIcon onClick={handleOpenNotifications}/>
+                            </Badge>
+                        </IconButton>
+                        <IconButton
+                            edge="end"
+                            aria-label="account of current user"
+                            aria-controls={menuId}
+                            aria-haspopup="true"
+                            onClick={handleProfileMenuOpen}
+                            color="inherit"
+                        >
+                            <AccountCircle />
+                        </IconButton>
+                    </div>
+                </Toolbar>
+            </AppBar>
+            {renderMenu}
+        </div>
+    );
+}
